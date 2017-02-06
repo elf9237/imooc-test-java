195aa0cf34b52f4e472a64dd0dab4bdc74e40cca
Andy <565071984@qq.com>
2017-02-06 17:56:02 +0800

    继承测试

---
+++ imooc/src/project/Animal.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Animal {
+	public int age;
+	public String name;
+	public void eat(){
+		System.out.println("动物有吃东西的能力！");
+	}	
+}
--- imooc/src/project/initialProject03.java
+++ imooc/src/project/initialProject03.java
@@ -4,7 +4,7 @@ import project.second.Project03;
 //import project.second.*;表示使用子包中所有的类
 public class initialProject03 {
 	public static void main(String[] args) {
-		Project03 show01 = new Project03();
+		//Project03 show01 = new Project03();
 		Project03 show02 = new Project03(5.0f,1.4f,2.0f);
 		System.out.println(show02.getScreen());
 		show02.setScreen(6.0f);
